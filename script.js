ef8a3e742fd1fb25096246c5e410b02bd925886e
Matteo Ceserani <ceserani@gmail.com>
2023-04-13 15:53:13 +0200

    Minor changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ products.forEach(product => {
   const quantity = product.querySelector('input');
 
   button.addEventListener('click', function() {
-    const name = product.querySelector('h2').textContent;
+    const name = product.querySelector('h5').textContent;
     const price = parseFloat(product.querySelector('p:last-of-type').textContent.slice(8));
     const image = product.querySelector('img').src;
     const quantityValue = parseInt(quantity.value);
